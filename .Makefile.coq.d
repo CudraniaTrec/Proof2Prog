a1ceea0dd9105544a5f8d7b13662b03405f33b53
ZhechongHuang <cudraniatrec@163.com>
2024-03-27 15:57:00 +0800

    start translating from java to coq

--- .Makefile.coq.d
+++
@@ -1,6 +0,0 @@
-coq_code/Maps.vo coq_code/Maps.glob coq_code/Maps.v.beautified coq_code/Maps.required_vo: coq_code/Maps.v 
-coq_code/Maps.vio: coq_code/Maps.v 
-coq_code/Maps.vos coq_code/Maps.vok coq_code/Maps.required_vos: coq_code/Maps.v 
-coq_code/Syntax.vo coq_code/Syntax.glob coq_code/Syntax.v.beautified coq_code/Syntax.required_vo: coq_code/Syntax.v coq_code/Maps.vo
-coq_code/Syntax.vio: coq_code/Syntax.v coq_code/Maps.vio
-coq_code/Syntax.vos coq_code/Syntax.vok coq_code/Syntax.required_vos: coq_code/Syntax.v coq_code/Maps.vos
