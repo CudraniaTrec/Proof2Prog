c4ab0f369b2819f4ad2c99277e77dfad6698e711
cudraniatrec <cudraniatrec@163.com>
2024-04-16 02:00:27 +0000

    add map entry

--- coq_code/Syntax.v
+++ coq_code/Syntax.v
@@ -713,6 +713,9 @@ Definition have_right_conversion (Ty1 : Ty)(STy1 : STy) :=
                     | STyExternal "Object" => Some(STyString)
                     | _ => None
                     end
+    | TyExternal "List" => match STy1 with
+                            | STyExternal "Object" => Some(TyGeneric0 "List")
+                            end
     | _ => None
     end.
 Definition CObject : partial_map STy := (
@@ -987,11 +990,15 @@ Definition CMap (ST1 ST2:STy) : partial_map STy := (
                                                              "putIfAbsent"|-> <([ST1;ST2]->ST2)>;
                                                              "remove"|-><([ST1]->ST2)>;
                                                              "size"|-><([]->STyNumeric)>).
+Definition CMapEntry (ST1 ST2:STy) : partial_map STy := (   "getKey"|-> <([]->ST1)>;
+                                                             "getValue"|-> <([]->ST2)>).
 Definition CHashMap (ST1 ST2:STy) : partial_map STy := (     "containsKey"|-><([ST1]->STyBoolean)>;
                                                              "values"|-><([]->(STyGeneric1 "Collection" ST2))>;
                                                              "getOrDefault"|-><([ST1;ST2]->ST2)>;
                                                              "keySet"|-><([]->(STyGeneric1 "Set" ST1))>;
-                                                             "get"|-><([ST1]->ST2)>; 
+                                                             "get"|-><([ST1]->ST2)>;         
+                                                             "size"|-><([]->STyNumeric)>;                                       
+                                                             "entrySet" |-> <([]->(STyGeneric1 "Set" (STyGeneric2 "Map.Entry" ST1 ST2)))>;
                                                              "putAll"|-><([(STyGeneric2 "Map" ST1 ST2)]->STyVoid)>;
                                                              "put"|-><([ST1;ST2]->[])>).
 Definition CLinkedHashMap (ST1 ST2:STy) : partial_map STy := ( "putIfAbsent"|-><([ST1;ST2]->ST2)>;
@@ -1054,6 +1061,7 @@ Definition getClassMap (sty:STy)(Gamma: partial_map STy) : option(partial_map ST
                             | "Map" => Some (CMap T1 T2)
                             | "HashMap" => Some (CHashMap T1 T2)
                             | "LinkedHashMap" => Some (CLinkedHashMap T1 T2)
+                            | "Map.Entry" => Some (CMapEntry T1 T2)
                             | _ => None
                             end
     | STyArray T => Some (CBuiltInArray T)
@@ -1526,7 +1534,7 @@ End Syntax_Example.
 Open Scope string_scope.
 
 Example prog_well_typed : exists p, program_well_typed p.
-Proof with (simpl;try(reflexivity)).
+Proof.
 unfold program_well_typed.
 eexists.
 eexists.
@@ -1538,129 +1546,60 @@ eapply T_ProgramConcat.
     { eapply T_ImportDecl with(n1:="java.util")(n2:="*"). }
     { eapply T_ProgramConcat.
       { eapply T_ImportDecl with(n1:="java.math")(n2:="*"). }
-      { eapply T_ClassDecl with(name:="ExtractMinMax").
+      { eapply T_ClassDecl with(name:="EmptyDit").
         { simpl. try reflexivity. }
-        { eapply T_MethodDecl with(modif:="public static")(T:=(TyGeneric1 "List" TyInt))(m:="extractMinMax").
+        { eapply T_MethodDecl with(modif:="public static")(T:=TyBool)(m:="emptyDit").
           { simpl. try reflexivity. }
           { eapply T_Concat.
-            { eapply T_DeclNoInit with(T:=(TyGeneric1 "List" TyInt))(x:="testTup").
+            { eapply T_DeclNoInit with(T:=(TyExternal "Object"))(x:="list1").
               simpl. try reflexivity. }
-            { eapply T_Concat.
-              { eapply T_DeclNoInit with(T:=TyInt)(x:="k").
-                simpl. try reflexivity. }
-              { eapply T_Skip. } } }
+            { eapply T_Skip. } }
           { simpl. try reflexivity. }
           { eapply T_Concat.
             { eapply T_Concat.
-              { eapply T_DeclInit with(T:=(TyGeneric1 "List" TyInt))(x:="res").
+              { eapply T_DeclInit with(T:=TyBool)(x:="emptyDit").
                 { simpl. try reflexivity. }
-                { eapply T_New' with(T:=(TyGeneric1 "ArrayList" TyInt)).
-                  { simpl. try reflexivity. }
-                  { eapply T_Nil. } }
+                { eapply T_True. }
                 { simpl. try reflexivity. } }
               { eapply T_Skip. } }
             { eapply T_Concat.
-              { eapply T_Concat.
-                { eapply T_DeclInit with(T:=(TyGeneric1 "List" TyInt))(x:="temp").
-                  { simpl. try reflexivity. }
-                  { eapply T_New' with(T:=(TyGeneric1 "ArrayList" TyInt)).
-                    { simpl. try reflexivity. }
-                    { eapply T_List.
-                      { eapply T_Var with(x:="testTup").
-                        simpl. try reflexivity. }
-                      { eapply T_Nil. } } }
-                  { simpl. try reflexivity. } }
-                { eapply T_Skip. } }
-              { eapply T_Concat.
-                { eapply T_Expression.
-                  eapply T_MethodInvocation with(m:="sort").
-                  { eapply T_Var with(x:="temp").
-                    simpl. try reflexivity. }
-                  { simpl. try reflexivity. }
-                  { simpl. try reflexivity. }
-                  { eapply T_List.
-                    { eapply T_TyMethodInvocation with(m:="naturalOrder")(T:=(TyExternal "Comparator")).
-                      { simpl. try reflexivity. }
-                      { simpl. try reflexivity. }
-                      { eapply T_Nil. }
-                      { simpl. try reflexivity. } }
-                    { eapply T_Nil. } }
-                  { simpl. try reflexivity. } }
+              { eapply T_If.
+                { eapply T_InstanceOf with(T:=(TyExternal "List")).
+                  eapply T_Var with(x:="list1").
+                  simpl. try reflexivity. }
                 { eapply T_Concat.
-                  { eapply T_For.
-                    { eapply T_Concat.
-                      { eapply T_DeclInit with(T:=TyInt)(x:="i").
-                        { simpl. try reflexivity. }
-                        { eapply T_Integer with(n:=0). }
-                        { simpl. try reflexivity. } }
-                      { eapply T_Skip. } }
-                    { eapply T_Lt.
-                      { eapply T_Var with(x:="i").
+                  { eapply T_Foreach with(T:=(TyExternal "Object"))(x:="obj").
+                    { simpl. try reflexivity. }
+                    { eapply T_Conversion with(T:=(TyExternal "List")).
+                      { eapply T_Var with(x:="list1").
                         simpl. try reflexivity. }
-                      { eapply T_MethodInvocation with(m:="size").
-                        { eapply T_Var with(x:="temp").
-                          simpl. try reflexivity. }
-                        { simpl. try reflexivity. }
-                        { simpl. try reflexivity. }
-                        { eapply T_Nil. }
-                        { simpl. try reflexivity. } }
                       { simpl. try reflexivity. } }
-                    { eapply T_PostInc.
-                      eapply T_Var with(x:="i").
-                      simpl. try reflexivity. }
+                    { simpl. try reflexivity. }
                     { eapply T_Concat.
                       { eapply T_If.
-                        { eapply T_Or.
-                          { eapply T_Lt.
-                            { eapply T_Var with(x:="i").
-                              simpl. try reflexivity. }
-                            { eapply T_Var with(x:="k").
-                              simpl. try reflexivity. }
-                            { simpl. try reflexivity. } }
-                          { eapply T_Ge.
-                            { eapply T_Var with(x:="i").
-                              simpl. try reflexivity. }
-                            { eapply T_Sub.
-                              { eapply T_MethodInvocation with(m:="size").
-                                { eapply T_Var with(x:="temp").
-                                  simpl. try reflexivity. }
-                                { simpl. try reflexivity. }
-                                { simpl. try reflexivity. }
-                                { eapply T_Nil. }
-                                { simpl. try reflexivity. } }
-                              { eapply T_Var with(x:="k").
-                                simpl. try reflexivity. }
-                              { simpl. try reflexivity. } }
-                            { simpl. try reflexivity. } } }
+                        { eapply T_Not.
+                          eapply T_InstanceOf with(T:=(TyExternal "Map")).
+                          eapply T_Var with(x:="obj").
+                          simpl. try reflexivity. }
                         { eapply T_Concat.
                           { eapply T_Expression.
-                            eapply T_MethodInvocation with(m:="add").
-                            { eapply T_Var with(x:="res").
+                            eapply T_Assign.
+                            { eapply T_Var with(x:="emptyDit").
                               simpl. try reflexivity. }
-                            { simpl. try reflexivity. }
-                            { simpl. try reflexivity. }
-                            { eapply T_List.
-                              { eapply T_MethodInvocation with(m:="get").
-                                { eapply T_Var with(x:="temp").
-                                  simpl. try reflexivity. }
-                                { simpl. try reflexivity. }
-                                { simpl. try reflexivity. }
-                                { eapply T_List.
-                                  { eapply T_Var with(x:="i").
-                                    simpl. try reflexivity. }
-                                  { eapply T_Nil. } }
-                                { simpl. try reflexivity. } }
-                              { eapply T_Nil. } }
+                            { eapply T_False. }
                             { simpl. try reflexivity. } }
-                          { eapply T_Skip. } } }
+                          { eapply T_Concat.
+                            { eapply T_Break. }
+                            { eapply T_Skip. } } } }
                       { eapply T_Skip. } } }
-                  { eapply T_Concat.
-                    { eapply T_Return.
-                      { eapply T_Var with(x:="res").
-                        simpl. try reflexivity. }
-                      { simpl. try reflexivity. }
-                      { simpl. try reflexivity. } }
-                    { eapply T_Skip. } } } } } } } } } } }
+                  { eapply T_Skip. } } }
+              { eapply T_Concat.
+                { eapply T_Return.
+                  { eapply T_Var with(x:="emptyDit").
+                    simpl. try reflexivity. }
+                  { simpl. try reflexivity. }
+                  { simpl. try reflexivity. } }
+                { eapply T_Skip. } } } } } } } } }
   Unshelve.
   all: apply STyVoid.
 Defined.
